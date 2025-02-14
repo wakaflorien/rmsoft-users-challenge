71ea6d6aef6ec7775d8adcae94576a024c22ae6b
Florien Niyongabo <waka.florien45@gmail.com>
2025-02-14 19:03:11 +0200

    Add DynamicThemeProvider and theme context for dark/light mode support

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
+import { useTheme } from '@/app/contexts/ThemeProvider';
 import React from 'react'
 
 export default function Footer() {
+    const { theme, toggleTheme } = useTheme();
     return (
-        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-            <p className="text-black">&copy; Florien 2025</p>
+        <footer className={`row-start-3 flex gap-6 flex-wrap items-center justify-center`}>
+            <p className={`text-black ${theme === "dark" && "!text-white "}`}>&copy; Florien 2025</p>
         </footer>
     )
 }
